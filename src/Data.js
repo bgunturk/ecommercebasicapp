6190e6e01c5a393cc1ac20fa549ec021a1ccfb3d
bgunturk <bulent.gunturk@gmail.com>
2021-03-31 15:43:02 +0300

    contextFuntions

---
+++ src/Data.js
@@ -0,0 +1,32 @@
+export const dataData = [
+    {
+        id: 1,
+        title: 'Elma',
+        img: 'images/elma.jpg',
+        price: 11.99,
+        section: "Meyve",
+        info: "Greatest food ever",
+        inCart: false,
+        count: 1
+    },
+    {
+        id: 2,
+        title: 'Elma',
+        img: 'images/elma.jpg',
+        price: 11.99,
+        section: "Meyve",
+        info: "Greatest food ever",
+        inCart: false,
+        count: 1
+    },
+    {
+        id: 3,
+        title: 'Elma',
+        img: 'images/elma.jpg',
+        price: 11.99,
+        section: "Meyve",
+        info: "Greatest food ever",
+        inCart: false,
+        count: 1
+    }
+]
